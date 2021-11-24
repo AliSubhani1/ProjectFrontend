560b6b17a506d0a7aa9a2ce618315e311b1e06f7
Ali Subhani <ali.subhani@tintash.com>
2021-11-24 17:36:34 +0500

    codding challeneg 3

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
-let marksWeight1 = 78;
-let marksHeight1 = 1.69;
-let johnsWeight1 = 92;
-let johnsHeight1 = 1.95;
+let ScoreDolphinA = 96;
+let ScoreDolphinB = 108;
+let ScoreDolphinC = 89;
+let ScoreKoalasA = 88;
+let ScoreKoalasB = 91;
+let ScoreKoalasC = 110;
 
-let marksWeight2 = 95;
-let marksHeight2 = 1.88;
-let johnsWeight2 = 85;
-let johnsHeight2 = 1.76;
-
-function calculateBmi(weight, height) {
-  return weight / (height * height);
-}
-let marksFirstBmi = calculateBmi(marksWeight1, marksHeight1);
-let johnsFirstBmi = calculateBmi(johnsWeight1, johnsHeight1);
-let marksSecondBmi = calculateBmi(marksWeight2, marksHeight2);
-let johnsSecondBmi = calculateBmi(johnsWeight2, johnsHeight2);
-console.log("Test Data 1");
-console.log("Marks BMI=", marksFirstBmi);
-console.log("Johns BMI=", johnsFirstBmi);
-console.log("Test Data 2");
-console.log("Marks BMI=", marksSecondBmi);
-console.log("Johns BMI=", johnsSecondBmi);
-let markHigherBmi1 = marksFirstBmi > johnsFirstBmi;
-console.log(markHigherBmi1);
-let markHigherBmi2 = marksSecondBmi > johnsSecondBmi;
-console.log(markHigherBmi2);
-if (marksFirstBmi > johnsFirstBmi) {
-  console.log(
-    `Marks BMI is (${marksFirstBmi}) high than Johns(${johnsFirstBmi})`
-  );
+let dolphinAvg = (ScoreDolphinA + ScoreDolphinB + ScoreDolphinC) / 3;
+let KoalasAvg = (ScoreKoalasA + ScoreKoalasB + ScoreKoalasC) / 3;
+if (dolphinAvg === KoalasAvg && dolphinAvg > 100) {
+  console.log("It is a draw");
+} else if (dolphinAvg > KoalasAvg && dolphinAvg > 100) {
+  console.log("Dolphin is winner");
+} else if (KoalasAvg > dolphinAvg && KoalasAvg > 100) {
+  console.log("Koalas is winner.");
 } else {
-  console.log("Johns bmi is higher than marks");
+  console.log("score is less than 100");
 }
