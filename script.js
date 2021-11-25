15b1e423dd8179f9449e2fd1b55e73b52881ee00
Ali Subhani <ali.subhani@tintash.com>
2021-11-25 09:56:12 +0500

    coding challenege arrow functions

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-"use strict";
-
-const calcAge3 = (result) => 2021 - birthYear;
-let birthYear = 1994;
-const age = calcAge3(birthYear);
-console.log(age);
-
-const yearsUntillRetirement = (birth) => {
-  const age = 2021 - birth;
-  let retirementYears = 65 - age;
-  return retirementYears;
+averageScore = (Score1, score2, score3) => (Score1 + score2 + score3) / 3;
+//Test 1
+DolphinsScore = averageScore(44, 23, 71);
+KoalasScore = averageScore(65, 54, 49);
+checkWinner = (DolphinAvgScore, KoalasAvgScore) => {
+  if (
+    DolphinAvgScore > KoalasAvgScore &&
+    DolphinAvgScore >= KoalasAvgScore * 2
+  ) {
+    console.log(`Dolphin wins (${DolphinAvgScore}) vs (${KoalasAvgScore})`);
+  } else if (
+    KoalasAvgScore > DolphinAvgScore &&
+    KoalasAvgScore >= DolphinAvgScore * 2
+  ) {
+    console.log(`Koalas wins (${KoalasAvgScore}) vs (${DolphinAvgScore})`);
+  } else {
+    console.log("no team wins");
+  }
 };
-console.log(yearsUntillRetirement(1994));
+checkWinner(DolphinsScore, KoalasScore);
+checkWinner(576, 111);
+
+//Test 2
+DolphinsScore = averageScore(85, 54, 41);
+KoalasScore = averageScore(23, 34, 27);
+checkWinner(DolphinsScore, KoalasScore);
