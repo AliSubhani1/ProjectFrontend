be93675a197eea636428deb754521f3ed42d9146
Ali Subhani <ali.subhani@tintash.com>
2021-11-25 15:37:08 +0500

    coding challenge

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-const jonas = [
-  "Jonas",
-  "schmedtmann",
-  2037 - 1991,
-  "teacher",
-  ["Micheal", "Peter", "Steven"],
-  true,
-];
-const types = [];
-for (let i = 0; i < jonas.length; i++) {
-  console.log(jonas[i], typeof jonas[i]);
-  types[i] = typeof jonas[i];
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let total = [];
+function calcTip(bills) {
+  for (bill in bills) {
+    if (bill >= 50 && bill <= 200) {
+      tips.push(bills[bill] * 0.15);
+      total.push(bills[bill] * 0.15 + bills[bill]);
+    } else {
+      tips.push(bills[bill] * 0.2);
+      total.push(bills[bill] * 0.2 + bills[bill]);
+    }
+  }
 }
-console.log(types);
-
-console.log("----ONLY STRINGS------");
-
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] !== "string") continue;
-  console.log(jonas[i], typeof jonas[i]);
-}
-
-console.log("----BREAK WITH NUMBER------");
-
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] == "number") break;
-  console.log(jonas[i], typeof jonas[i]);
-}
+calcTip(bills);
+console.log("tips=", tips);
+console.log("totals=", total);
